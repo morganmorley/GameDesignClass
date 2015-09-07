0b9c4619cb035549009dcddda6e2b047aaf84dee
morganmorley <mmills@bennington.edu>
2015-09-07 17:31:08 -0400

    boxes are “eaten”

---
+++ FirstGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	//defines the movement of the player sphere
+
+	Rigidbody physicsbody;
+	int speed; //mult to vector to increase speed
+
+	// Use this for initialization
+	void Start () {
+		physicsbody = GetComponent<Rigidbody> ();
+		speed = 3;
+	}
+	
+	void FixedUpdate () {
+		float xMovement = Input.GetAxis("Horizontal");
+		float zMovement = Input.GetAxis ("Vertical");
+		physicsbody.AddForce(new Vector3(xMovement,0,zMovement) * speed); //movement along x and z axes depending on which button you push
+	}
+
+	void OnTriggerEnter(Collider cube){
+		//when roll through a box, it "triggers" this function
+		cube.gameObject.SetActive (false); //this sets cube inactive and it disappears
+	}
+}
