a7b4b838aa744991179dea5032f9b48097c0f421
morganmorley <mmills@bennington.edu>
2015-09-14 17:38:41 -0400

    First Game Final

--- FirstGame/Assets/Scripts/PlayerMovement.cs
+++ FirstGame/Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour {
 
 	//defines the movement of the player sphere
 
 	Rigidbody physicsbody;
-	int speed; //mult to vector to increase speed
+	public int speed; //mult to vector to increase speed
+	public Text scoreboard;
+	public Text youWin;
 	int count; //for scoreboard
-	string score;
 
 	// Use this for initialization
 	void Start () {
 		physicsbody = GetComponent<Rigidbody> ();
 		speed = 3;
 		count = 0;
+		UpdateScore (count);
+		youWin.gameObject.SetActive (false);
 	}
 	
 	void FixedUpdate () {
@@ -26,8 +30,15 @@ public class PlayerMovement : MonoBehaviour {
 	void OnTriggerEnter(Collider cube){
 		//when roll through a box, it "triggers" this function
 		cube.gameObject.SetActive (false); //this sets cube inactive and it disappears
-		count = count + 1;
-		score = "Score: " + count.ToString ();
-		Debug.Log (score);
+		count++;
+		UpdateScore (count);
+	}
+
+	void UpdateScore (int newScore) {
+		string score = "Score: " + newScore.ToString ();
+		scoreboard.text = score;
+		if (count > 3) {
+			youWin.gameObject.SetActive(true);
+		}
 	}
 }
