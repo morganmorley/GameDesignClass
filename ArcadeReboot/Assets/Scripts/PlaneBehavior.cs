aa2ce9bfa8c54ca438f21f71048a5faf9e20f637
morganmorley <mmills@bennington.edu>
2015-09-24 12:41:13 -0400

    Rotate Works, Needs Collider

--- ArcadeReboot/Assets/Scripts/PlaneBehavior.cs
+++ ArcadeReboot/Assets/Scripts/PlaneBehavior.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlaneBehavior : MonoBehaviour {
+
+	Rigidbody planeRigidbody;
+	public float movementSpeed = 6f; //= 6.0
+	public float turnSpeed = 65f;
 
 	// Use this for initialization
 	void Start () {
-	
+		planeRigidbody = GetComponent <Rigidbody> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		float zRotation = Input.GetAxis ("Vertical");
+		float xRotation = Input.GetAxis ("Horizontal");
+
+		float currentRotationZ = transform.rotation.eulerAngles.z;
+		float currentRotationX = transform.rotation.eulerAngles.x;
+		float targetAngleZ;
+		float targetAngleX;
+
+
+		if (zRotation < 45 && zRotation > (-45)) {
+			targetAngleZ = currentRotationZ + zRotation * Time.deltaTime * turnSpeed;
+		} else {
+			targetAngleZ = currentRotationZ;
+		}
+
+		if (xRotation < 45 && xRotation > (-45)) {
+			targetAngleX = currentRotationX + xRotation *Time.deltaTime * turnSpeed;
+		} else {
+			targetAngleX = currentRotationX;
+		}
+
+		//Plane turning motion:
+		Quaternion newRotation = Quaternion.Euler (new Vector3 (targetAngleX, 0, targetAngleZ));
+		planeRigidbody.MoveRotation (newRotation); 
 	}
 }
+	
---
+++ SurvivalGameDemo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody playerRigidbody;
+	Animator playerAnimator;
+
+	public float movementSpeed = 6f; //= 6.0
+	public float turnSpeed = 65f;
+
+	// Use this for initialization
+	void Start () {
+		playerRigidbody = GetComponent <Rigidbody> ();
+		playerAnimator = GetComponent <Animator> ();
+	
+	}
+	
+	// because some frames take longer than others, so multiply movement with Time.deltaTime
+	void Update () {
+		//Input from arrow keys:
+		float forwardMovement = Input.GetAxis ("Vertical");
+		float turnMovement = Input.GetAxis ("Horizontal");
+
+		//Whether player is "walking" or "idle"
+		if (forwardMovement == 0 && turnMovement == 0) {
+			playerAnimator.SetBool ("IsWalking", false); //links to animator to make player idle, etc.
+		} else {
+			playerAnimator.SetBool ("IsWalking", true);
+		}
+
+		//Player forward motion:
+		Vector3 movement = forwardMovement * transform.forward; //makes player move forward
+		movement = movement.normalized * Time.deltaTime * movementSpeed; //normalizes movement vector so don't move faster diagonally
+		playerRigidbody.MovePosition (transform.position + movement);
+
+		//Player turning motion:
+		float currentRotation = transform.rotation.eulerAngles.y;
+		float targetAngle = currentRotation + turnMovement * Time.deltaTime * turnSpeed;
+		Quaternion newRotation = Quaternion.Euler (new Vector3 (0, targetAngle, 0));
+		playerRigidbody.MoveRotation (newRotation); //turns the player by targetAngle in Quaternions
+
+	}
+}
