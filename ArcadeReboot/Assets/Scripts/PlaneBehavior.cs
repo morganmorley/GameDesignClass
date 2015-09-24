674fea9c8f0afb6a6e8489f5111f8a7392ad399e
morganmorley <mmills@bennington.edu>
2015-09-24 15:54:04 -0400

    Should I use a capsule collider? The ball is floating.

--- ArcadeReboot/Assets/Scripts/PlaneBehavior.cs
+++ ArcadeReboot/Assets/Scripts/PlaneBehavior.cs
@@ -1,21 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class PlaneBehavior : MonoBehaviour {
 
 	Rigidbody planeRigidbody;
 	public float movementSpeed = 6f; //= 6.0
 	public float turnSpeed = 65f;
+	public Text youWin; //the notification of successfully eating 4 boxes
+
 
 	// Use this for initialization
 	void Start () {
 		planeRigidbody = GetComponent <Rigidbody> ();
+		youWin.gameObject.SetActive(false);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		float zRotation = Input.GetAxis ("Vertical");
-		float xRotation = Input.GetAxis ("Horizontal");
+		float xRotation = Input.GetAxis ("Vertical");
+		float zRotation = Input.GetAxis ("Horizontal");
 
 		float currentRotationZ = transform.rotation.eulerAngles.z;
 		float currentRotationX = transform.rotation.eulerAngles.x;
@@ -38,6 +43,14 @@ public class PlaneBehavior : MonoBehaviour {
 		//Plane turning motion:
 		Quaternion newRotation = Quaternion.Euler (new Vector3 (targetAngleX, 0, targetAngleZ));
 		planeRigidbody.MoveRotation (newRotation); 
+
+
+	}
+
+	void OnTriggerEnter(Collider cube){
+		//when roll through a box, it "triggers" this function
+		cube.gameObject.SetActive (false); //this sets cube inactive and it disappears
+		youWin.gameObject.SetActive(true);
 	}
 }
 	
